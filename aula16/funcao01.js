a9476709e73fc79442a5499699877ea7d3d6dbcb
Cláudio Santana <claudio_bs@icloud.com>
2023-12-14 09:01:15 -0300

    feat: add aula 16 - Funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+console.log(parimpar(11))
+//let res = parimpar(4) => Outra maneira de chamar a função.
+//console.log(res)
---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // Se os parâmetros não forem declarados n1 e n2 receberam 0.
+    return n1 + n2
+}
+
+console.log(soma(7))
